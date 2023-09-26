a56e0aae49838c90a30f691efed4c6ce6adbced2
Alex-gikungu <alexigikungu.012@gmail.com>
2023-09-26 09:01:28 +0300

    added app file imports all the files needed to run the api and also the migrations

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# from flask import Flask
-# from config import Config
-# from models import db
-# from flask_migrate import Migrate
+from flask import Flask
+from config import Config
+from models import db
+from flask_migrate import Migrate
 
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db.init_app(app)
-# migrate = Migrate(app, db)
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
-# # Import and register routes from routes.py
-# from routes import *
+# Import and register routes from routes.py
+from routes import *
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
